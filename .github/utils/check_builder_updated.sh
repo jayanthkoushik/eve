eed4a29b3ab42023f17e7a387fc9a9d099ba0331
Jayanth Koushik <jnkoushik@gmail.com>
2020-05-31 19:49:57 -0400

    whoops

--- .github/utils/check_builder_updated.sh
+++ .github/utils/check_builder_updated.sh
@@ -1,3 +1,3 @@
 lastref=`cat .github/utils/last_build_ref | tr -d '[:space:]'`
-curref=`git ls-remote git@github.com:jayanthkoushik/build-webpage HEAD | cut -f1`
+curref=`git ls-remote https://github.com/jayanthkoushik/build-webpage HEAD | cut -f1`
 test $lastref != $curref
