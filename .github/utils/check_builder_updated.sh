ddce6b21b2dc54b1c4f1a385c195ee2f0db33f38
Jayanth Koushik <jnkoushik@gmail.com>
2020-05-31 20:57:30 -0400

    It was easy all along

--- .github/utils/check_builder_updated.sh
+++
@@ -1,7 +0,0 @@
-lastref=`cat .github/utils/last_build_ref | tr -d '[:space:]'`
-curref=`git ls-remote https://github.com/jayanthkoushik/build-webpage HEAD | cut -f1`
-if [ "$lastref" = "$curref" ]; then
-    echo '::set-ouput name=updated::0'
-else
-    echo '::set-ouput name=updated::1'
-fi
