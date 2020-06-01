d9c0dc26047843b328c5bc07a48572710841cae9
Jayanth Koushik <jnkoushik@gmail.com>
2020-05-31 20:18:22 -0400

    FUCK. sh.

--- .github/utils/check_builder_updated.sh
+++ .github/utils/check_builder_updated.sh
@@ -1,8 +1,7 @@
 lastref=`cat .github/utils/last_build_ref | tr -d '[:space:]'`
 curref=`git ls-remote https://github.com/jayanthkoushik/build-webpage HEAD | cut -f1`
 if [ "$lastref" = "$curref" ]; then
-    updated=0
+    echo '::set-ouput name=updated::0'
 else
-    updated=1
+    echo '::set-ouput name=updated::1'
 fi
-echo '::set-ouput name=updated::${updated}'
